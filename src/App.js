ce8e629f6abc2760531b2de7941e7fd2c4a90954
Dhinesh Dhamodharan <dhineshdhamodharan@gmail.com>
2021-09-05 15:46:32 +0530

    updatefile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,127 @@
-import logo from "./logo.svg";
-import "./index.css";
-import Todo from "./Todo";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [todolist, setTodolist] = useState([]);
+  const [tasks, setTask] = useState("");
+  useEffect(async () => {
+    fetchtask();
+  }, []);
+
+  let fetchtask = async () => {
+    try {
+      let todolist = await axios.get(
+        "https://apptodonodejs.herokuapp.com/todolist"
+      );
+      console.log(todolist);
+      setTodolist([...todolist.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let handleCreateTask = async () => {
+    try {
+      let postData = await axios.post(
+        "https://apptodonodejs.herokuapp.com/create-task",
+        {
+          tasks,
+        }
+      );
+      fetchtask();
+      setTask("");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  let handleChange = async (e, id) => {
+    try {
+      let putData = await axios.put(
+        `https://apptodonodejs.herokuapp.com/update-task/${id}`,
+        {
+          status: e.target.checked,
+        }
+      );
+      fetchtask();
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(id);
+  };
+
+  let handleDelete = async (id) => {
+    try {
+      await axios.delete(
+        `https://apptodonodejs.herokuapp.com/delete-task/${id}`
+      );
+      fetchtask();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route path="/todo">
-            <Todo />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <h4>To do Lists</h4>
+          <div className="col-lg-12">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                value={tasks}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Add tasks"
+                aria-label=""
+                aria-describedby="button-addon2"
+              />
+              <button
+                onClick={handleCreateTask}
+                class="btn btn-primary"
+                type="button"
+                id="button-addon2"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <ul class="list-group">
+            {todolist.map((item) => {
+              return (
+                <li class="list-group-item">
+                  <input
+                    class="form-check-input me-1"
+                    type="checkbox"
+                    value=""
+                    aria-label="..."
+                    onChange={(e) => handleChange(e, item._id)}
+                    checked={item.status}
+                  />
+                  <span
+                    style={{
+                      textDecoration: item.status ? "line-through" : "",
+                    }}
+                  >
+                    {item.tasks}
+                  </span>
+                  <button
+                    type="button"
+                    class="btn btn-danger"
+                    onClick={() => handleDelete(item._id)}
+                  >
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
+
 export default App;
